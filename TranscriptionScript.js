b72f78c026b8e0f55b29066db961f080a274e996
Canh_Dev <canh.hoangdinh@skybull.studio>
2025-06-19 17:00:29 +0700

    test resolutoion

--- TranscriptionScript.js
+++ TranscriptionScript.js
@@ -1,80 +1,64 @@
-let recognization = new webkitSpeechRecognition();
+﻿let recognization = new webkitSpeechRecognition();
+let isRecognizing = false;
+let GameObjName = null;
 
-var result = false;
-var stop = true; // Initially stopped
-var started = false; // Recognition has not started yet
 recognization.continuous = true;
 recognization.interimResults = true;
-var GameObjName = null;
 
 function SetGameObjectName(name) {
     GameObjName = name;
-    console.log(GameObjName);
+    console.log("GameObject for result:", GameObjName);
 }
 
 function runspeechrecognition(listenContinuous) {
+    if (isRecognizing) {
+        console.log("Recognition already running.");
+        return;
+    }
+
     recognization.onstart = () => {
         console.log("Speech recognition started");
-        started = true;
-        result = false;
-        stop = false;
+        isRecognizing = true;
     };
 
     recognization.onresult = (e) => {
-        //var transcript = e.results[0][0].transcript;
-        //console.log("Speech recognition result:", transcript);
-
-        //window.Gameinstance.SendMessage(GameObjName, "Result", transcript);
-        //result = true;
-
-        //if (listenContinuous && !stop) {
-        //    setTimeout(() => {
-        //        if (!started) {
-        //            recognization.start(); // Restart recognition
-        //        }
-        //    }, 100);
-        //}
         let transcript = "";
         for (let i = e.resultIndex; i < e.results.length; ++i) {
             transcript += e.results[i][0].transcript;
         }
 
         console.log("Transcript:", transcript);
-        window.Gameinstance.SendMessage(GameObjName, "Result", transcript);
-        result = true;
+        if (GameObjName) {
+            window.Gameinstance.SendMessage(GameObjName, "Result", transcript);
+        }
     };
 
     recognization.onend = () => {
         console.log("Speech recognition ended");
-        started = false;
+        isRecognizing = false;
 
-        //if (!result && listenContinuous && !stop) {
-        //    setTimeout(() => {
-        //        if (!started) {
-        //            recognization.start(); // Restart recognition
-        //        }
-        //    }, 100);
-        //}
-
-        if (listenContinuous && !stop) {
+        if (listenContinuous) {
             setTimeout(() => {
-                if (!started) recognization.start();
-            }, 100);
+                if (!isRecognizing) {
+                    recognization.start(); // restart
+                }
+            }, 300); // delay an chút để tránh spam
         }
     };
 
-    // Start recognition if it is not running and has not been stopped
-    if (!started && stop) {
-        stop = false;
+    try {
         recognization.start();
+    } catch (e) {
+        console.warn("Recognition start error:", e);
     }
 }
 
 function stoprecognition() {
-    console.log("Stopping speech recognition");
-    recognization.stop();
-    stop = true;
-    started = false;
+    if (isRecognizing) {
+        recognization.stop();
+        isRecognizing = false;
+        console.log("Speech recognition stopped.");
+    }
 }
 
 function downloadfile(fileName, content) {
